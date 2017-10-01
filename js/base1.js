20bc907a478b3de6ed2d627f924550015d516b88
Himesh Jain <himeshjain13@gmail.com>
2017-10-01 16:34:11 +0530

    Add Skill Section, Fix bugs

--- js/base1.js
+++ js/base1.js
@@ -72,33 +72,162 @@ $(document).ready(function () {
     console.log(parseInt($(window).scrollTop()/window.innerHeight));
 });
 
+if (window.innerWidth < 1000) {
+    $("#pcol").css("opacity",1);
+}
+
 var scrollPos;
 $(window).scroll(function(){
     scrollPos = $(window).scrollTop();  
-    if (scrollPos >= $("#profile").offset().top && scrollPos < $("#profile").offset().top + window.innerHeight) {
+    if (scrollPos >= $("#profile").offset().top && scrollPos < $("#profile").offset().top + window.innerHeight && window.innerWidth >= 1000) {
         $("#pcol").addClass('sOutL');
         $("#pimg").addClass('sOutR');
     }
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    // Get all "navbar-burger" elements
+    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+        // Add a click event on each of them
+        $navbarBurgers.forEach(function ($el) {
+            $el.addEventListener('click', function () {
+                // Get the target from the "data-target" attribute
+                var target = $el.dataset.target;
+                var $target = document.getElementById(target);
+                // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+                $el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+            });
+        });
+    }
+});
 
 /*=========================
     Keyboard Shortcuts
 =========================*/
 document.addEventListener('keyup', function(event) {
-    if (event.which === 40) {
-        dcount = parseInt($(window).scrollTop()/window.innerHeight);            
-        if ($(window).scrollTop() < pageHeight - window.innerHeight) dcount++;
-        console.log(dcount);
-        $('html, body').animate({
-            scrollTop: window.innerHeight*dcount
-        }, 800);
+    if(dcount < 2) {
+        if (event.which === 40) {
+            dcount = parseInt($(window).scrollTop()/window.innerHeight);            
+            if (($(window).scrollTop() < pageHeight - window.innerHeight)&&dcount<2) dcount++;
+            console.log(dcount);
+            $('html, body').animate({
+                scrollTop: window.innerHeight*dcount
+            }, 800);
+        }
     }
-    if (event.which === 38) {
-        if (dcount>0) dcount--;
-        $('html, body').animate({
-            scrollTop: window.innerHeight*dcount
-        }, 800);
+    if ($(window).scrollTop() < window.innerHeight*2) {
+        if (event.which === 38) {
+            if (dcount>0) dcount--;
+            $('html, body').animate({
+                scrollTop: window.innerHeight*dcount
+            }, 800);
+        }
     }
 });
 
+/*=========================
+    Skill Section
+=========================*/
+
+var skills = [
+    { 
+        "header" : "INTERESTS",
+        "captions" : ["Reading", "Web", "Mobile", "Design", "Movies"],
+        "values" : [0.70, 0.90, 0.80, 0.85, 0.90 ]
+    },
+    {
+        "header" : "LANGUAGES",
+        "captions" : ["C", "HTML", "JS", "Ruby", "Java"],
+        "values" : [0.80, 0.85, 0.90, 0.70, 0.90]
+    },
+    {
+        "header" : "MISC",
+        "captions" : ["VS Code", "Git", "Photoshop", "DBMS", "Linux"],
+        "values" : [0.8, 0.85, 0.6, 0.75, 0.80]
+    }
+  ];
+  
+  var pentagonIndex = 0;
+  var valueIndex = 0;
+  var width = 0;
+  var height = 0;
+  var radOffset = Math.PI/2
+  var sides = 5; // Number of sides in the polygon
+  var theta = 2 * Math.PI/sides; // radians per section
+  
+  function getXY(i, radius) {
+    return {"x": Math.cos(radOffset +theta * i) * radius*width + width/2,
+      "y": Math.sin(radOffset +theta * i) * radius*height + height/2};
+  }
+  
+  var hue = [];
+  var hueOffset = 25;
+  
+  for (var s in skills) {
+    $(".content-skills").append('<div class="pentagon" id="interests"><div class="header-skills"></div><canvas class="pentCanvas"/></div>');
+    hue[s] = (hueOffset + s * 255/skills.length) % 255;
+  }
+  
+  $(".pentagon").each(function(index){
+    width = $(this).width();
+    height = $(this).height();
+    var ctx = $(this).find('canvas')[0].getContext('2d');
+    ctx.canvas.width = width;
+    ctx.canvas.height = height;
+    ctx.font="20px Raleway";
+    ctx.textAlign="center";
+    
+    /*** LABEL ***/
+    color = "hsl("+hue[pentagonIndex]+", 100%, 50%)";
+    ctx.fillStyle = color;
+    ctx.fillText(skills[pentagonIndex].header, width/2, 15);
+  
+    ctx.font="17px Raleway";   
+  
+    /*** PENTAGON BACKGROUND ***/
+    for (var i = 0; i < sides; i++) {
+      // For each side, draw two segments: the side, and the radius
+      ctx.beginPath();
+      xy = getXY(i, 0.3);
+      colorJitter = 25 + theta*i*2;
+      color = "hsl("+hue[pentagonIndex]+",100%," + colorJitter + "%)";
+      ctx.fillStyle = color;
+      ctx.strokeStyle = color;
+      ctx.moveTo(0.5*width, 0.5*height); //center
+      ctx.lineTo(xy.x, xy.y);
+      xy = getXY(i+1, 0.3);
+      ctx.lineTo(xy.x, xy.y);
+      xy = getXY(i, 0.37);
+      console.log();
+      ctx.fillText(skills[ pentagonIndex].captions[valueIndex],xy.x, xy.y +5);
+      valueIndex++;
+      ctx.closePath();
+      ctx.fill();
+      ctx.stroke();
+    }
+    
+    valueIndex = 0;
+    ctx.beginPath();
+    ctx.fillStyle = "rgba(0, 0, 0, 0.2)";
+    ctx.strokeStyle = "rgba(0, 0, 0, 0.3)";
+    ctx.lineWidth = 5;
+    var value = skills[pentagonIndex].values[valueIndex];
+    xy = getXY(i, value * 0.3);
+    ctx.moveTo(xy.x,xy.y);
+    /*** SKILL GRAPH ***/
+    for (var i = 0; i < sides; i++) {
+      xy = getXY(i, value * 0.3);
+      ctx.lineTo(xy.x,xy.y);
+      valueIndex++;
+      value = skills[pentagonIndex].values[valueIndex];
+    }
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fill();
+    valueIndex = 0;  
+    pentagonIndex++;
+  });
+  
